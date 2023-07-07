1b3b033b60b3012235e5827de992c96d8647a6e2
joan_the_narc <ryanbrogley@gmail.com>
2023-07-07 18:02:05 -0400

    Ryan scene

--- Assets/Scripts/Fisherman.cs
+++ Assets/Scripts/Fisherman.cs
@@ -10,7 +10,7 @@ public class Fisherman : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        //fake comment
     }
 
     // Update is called once per frame
