6568cb3fa2abf5bc6f67e77583c76dbc4942016b
Jessica Epperson <91020948+LegitTeddyBears@users.noreply.github.com>
2023-07-08 04:05:23 -0400

    Started New Input System
    Started New Input System and Added Camera Movement system

--- Assets/Scripts/Fish Movement.cs
+++ Assets/Scripts/Fish Movement.cs
@@ -3,20 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Fish : MonoBehaviour
+public class FishMovement : MonoBehaviour
 {
-
     public Rigidbody2D rb;
     public float turnSpeed = 1000;
-    public float moveSpeed = 4;
+    public float moveSpeed = 15;
+    public float movementX;
+    public float movementY;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void onMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
     void FixedUpdate()
     {
+
+        Vector2 movement = new Vector2();
+        rb.AddForce(movement * moveSpeed);
+
+        /*
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direction = mousePosition - transform.position;
         float angle = Vector2.SignedAngle(Vector2.right, direction);
         Vector3 targetRotation = new Vector3(0, 0, angle);
         rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), turnSpeed * Time.deltaTime));
         rb.MovePosition(rb.position + ((Vector2)transform.right * moveSpeed * Time.deltaTime));
-        // Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+        Camera.main.transform.position =new Vector3(transform.position.x,transform.position.y, -10);
+        */
     }
 }
