a4f73a2faa77d7ec805441cfeffc2e3ba606ffea
Coge12 <65317607+Coge12@users.noreply.github.com>
2023-07-07 22:18:51 -0400

    Updated Spawner
    Added boat spawning
    
    Made it so boats and rocks are also randomly rotated up spawn.

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class SpawnerScript : MonoBehaviour
 {
     //Object to spawn
     public GameObject rock;
+    public GameObject boat;
 
     //Spawn Boundries
     public float TopBoundry = 0;
@@ -20,8 +22,13 @@ public class SpawnerScript : MonoBehaviour
     public float MaxRocks = 0;
     private float CurrentRocks = 0;
 
+    //Boat Spawn Mechanism
+    public float spawnRate = 0;
+    private float spawnTime = 0;
+
     //Radius around rocks where no rocks can spawn
-    public float spawnRadius = 0;
+    public float spawnRadiusRock = 0;
+    public float spawnRadiusBoat = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +39,21 @@ public class SpawnerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        spawnRocks();
+        if (CurrentRocks < MaxRocks)
+        {
+            spawnRocks();
+        }
+        //UnityEngine.Debug.Log("Current spawn time: " + spawnTime);
+        if (spawnTime < spawnRate)
+        {
+            spawnTime += Time.deltaTime;
+        }
+        else
+        {
+            spawnTime = 0;
+            spawnBoat();
+        }
+
         
     }
 
@@ -41,16 +62,29 @@ public class SpawnerScript : MonoBehaviour
         if (CurrentRocks < MaxRocks)
         {
             var position = new Vector3(Random.Range(LeftBoundry, RightBoundry), Random.Range(BottomBoundry, TopBoundry), 0);
-            Collider2D spawnCollision = Physics2D.OverlapCircle(position, spawnRadius);
+            var rot = new Vector3(0, 0, Random.Range(-180, 180));
+            Collider2D spawnCollision = Physics2D.OverlapCircle(position, spawnRadiusRock);
             UnityEngine.Debug.Log("Trying to Spawn Rock " + CurrentRocks);
             if (spawnCollision == false)
             {
-                Instantiate(rock, position, transform.rotation);
+                Instantiate(rock, position, Quaternion.Euler(rot));
                 CurrentRocks++;
                 UnityEngine.Debug.Log("Spawning Rock " + CurrentRocks);
             }
 
 
+        }
+    }
+
+    void spawnBoat()
+    {
+        var position = new Vector3(Random.Range(LeftBoundry, RightBoundry), Random.Range(BottomBoundry, TopBoundry), 0);
+        var rot = new Vector3(0, 0, Random.Range(-180, 180));
+        Collider2D spawnCollision = Physics2D.OverlapCircle(position, spawnRadiusBoat);
+        if (spawnCollision == false)
+        {
+            Instantiate(boat, position, Quaternion.Euler(rot));
+            UnityEngine.Debug.Log("Spawning boat ");
         }
     }
 
