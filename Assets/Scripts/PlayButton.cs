db47bdd1329378b54356d7d72f869ad3fc021415
Coge12 <65317607+Coge12@users.noreply.github.com>
2023-07-09 01:58:08 -0400

    Start Menu functional

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public string gotoScene;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gotoScene);
+        //SceneManager.LoadScene("Nick");
+    }
+}
